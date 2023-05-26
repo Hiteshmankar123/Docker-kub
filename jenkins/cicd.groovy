fff9731766e5e44cf36233ec446ff0f67f16f206
Hitesh Mankar <hiteshmankar777@gmail.com>
2023-05-26 11:57:00 +0530

    --- Auto Git Commit ---

--- jenkins/cicd.groovy
+++ jenkins/cicd.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('Build') { 
             steps {  
-              echo "mav"       
+              echo "mvn successful"       
             }
         }
         stage('Test') { 
